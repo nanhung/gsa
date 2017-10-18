3f1e15e5511f9bc59242a2450b270c2372257b9f
nanhung <nhsieh@cvm.tamu.edu>
2017-10-18 09:07:40 -0500

    add owen result

--- ownv1.R
+++ ownv1.R
@@ -78,7 +78,7 @@ O.X.df <- function(n){
              X21 = rtri(n, CLC_AS*exp(lwr), CLC_AS*exp(upr), CLC_AS))
 }
 
-n = 32000
+n = 8000 # consider to use threshold = 0.1 to improve efficiency
 X1 <- O.X.df(n); X2 <- O.X.df(n); X3 <- O.X.df(n);
 O.S <- sobolowen(NULL, log(X1), log(X2), log(X3), conf = 0.95, nboot = 1000)
 O.S.APAP.df <- cbind(1, O.S$X)
@@ -187,3 +187,58 @@ save(apap.mo.df.1.1, apap.mo.df.1.2, apap.mo.df.1.3, apap.mo.df.1.4,
      file = "ownv1.rda")
 
 # system.time(source("ownv1.R"))
+
+if(!require(gplots)) {
+  install.packages("gplots"); require(gplots)} #heatmap.2
+
+main1<-do.call(cbind, list(apap.mo.df.1.1[,2], apap.mo.df.1.2[,2], apap.mo.df.1.3[,2], apap.mo.df.1.4[,2],
+                           apap.mo.df.1.5[,2], apap.mo.df.1.6[,2], apap.mo.df.1.7[,2], apap.mo.df.1.8[,2],
+                           apap.mo.df.2.1[,2], apap.mo.df.2.2[,2], apap.mo.df.2.3[,2], apap.mo.df.2.4[,2],
+                           apap.mo.df.2.5[,2], apap.mo.df.2.6[,2], apap.mo.df.2.7[,2], apap.mo.df.2.8[,2],
+                           apap.mo.df.3.1[,2], apap.mo.df.3.2[,2], apap.mo.df.3.3[,2], apap.mo.df.3.4[,2],
+                           apap.mo.df.3.5[,2], apap.mo.df.3.6[,2], apap.mo.df.3.7[,2], apap.mo.df.3.8[,2]))
+
+totl1<-do.call(cbind, list(apap.to.df.1.1[,2], apap.to.df.1.2[,2], apap.to.df.1.3[,2], apap.to.df.1.4[,2],
+                           apap.to.df.1.5[,2], apap.to.df.1.6[,2], apap.to.df.1.7[,2], apap.to.df.1.8[,2],
+                           apap.to.df.2.1[,2], apap.to.df.2.2[,2], apap.to.df.2.3[,2], apap.to.df.2.4[,2],
+                           apap.to.df.2.5[,2], apap.to.df.2.6[,2], apap.to.df.2.7[,2], apap.to.df.2.8[,2],
+                           apap.to.df.3.1[,2], apap.to.df.3.2[,2], apap.to.df.3.3[,2], apap.to.df.3.4[,2],
+                           apap.to.df.3.5[,2], apap.to.df.3.6[,2], apap.to.df.3.7[,2], apap.to.df.3.8[,2]))
+
+colnames(main1)<-c("APAP_0.5h", "APAP_1h", "APAP_1.5h", "APAP_2h",
+                   "APAP_4h", "APAP_6h", "APAP_8h", "APAP_12h",
+                   "APAP-G_0.5h", "APAP-G_1h", "APAP-G_1.5h", "APAP-G_2h",
+                   "APAP-G_4h", "APAP-G_6h", "APAP-G_8h", "APAP-G_12h",
+                   "APAP-S_0.5h", "APAP-S_1h", "APAP-S_1.5h", "APAP-S_2h",
+                   "APAP-S_4h", "APAP-S_6h", "APAP-S_8h", "APAP-S_12h")
+
+rownames(main1)<-c("Tg","Tp","CYP_Km","CYP_VmaxC","SULT_Km_apap","SULT_Ki",
+                   "SULT_Km_paps","SULT_VmaxC","UGT_Km","UGT_Ki",
+                   "UGT_Km_GA","UGT_VmaxC","Km_AG","Vmax_AG","Km_AS",
+                   "Vmax_AS","kGA_syn","kPAPS_syn","CLC_APAP","CLC_AG","CLC_AS")
+
+rownames(totl1)<-rownames(main1)
+colnames(totl1)<-colnames(main1)
+
+
+main2 <- as.data.frame(main1)
+totl2 <- as.data.frame(totl1)
+
+M0<-main2[,1:24]
+T0<-totl2[,1:24]
+
+M1<-as.matrix(scale(M0))
+T1<-as.matrix(scale(T0))
+
+
+heatmap.2(M1, cexRow=1.2, cexCol=1.2, col = bluered(100), margins=c(6,9),trace="none",srtCol=35,
+          density.info = 'histogram', scale = "none", keysize = 1.2, 
+          cellnote=round(M1, digits = 1),
+          notecol="black")
+
+pdf(file="ownv1.pdf", width = 12, height = 8)
+heatmap.2(T1, cexRow=1.2, cexCol=1.2, col = bluered(100), margins=c(6,9),trace="none",srtCol=35,
+          density.info = 'histogram', scale = "none", keysize = 1.2, 
+          cellnote=round(T1, digits = 1),
+          notecol="black")
+dev.off()
