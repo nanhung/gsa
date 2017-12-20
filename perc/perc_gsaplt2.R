e1572287b26564f9ec640dbfd1677227383b05f0
nanhung <nhsieh@cvm.tamu.edu>
2017-12-20 09:21:16 -0600

    mass balance check and revise cumulative

--- perc/perc_gsaplt2.R
+++ perc/perc_gsaplt2.R
@@ -25,9 +25,7 @@ sim4 <- as.data.frame(fread("perc.setpoint.out", head = T))
 EP.plt<-function(X1, X2, colval, main){
   sen<-log(X1[,ncol(setpt1.df)+colval], 10)
   ins<-log(X2[,ncol(setpt1.df)+colval], 10)
-  sen[sen < 0] <- NA # remove log value less than 1
-  sen<-na.omit(sen)
-  ins<-na.omit(ins)
+
   sen.ecdf <- ecdf(sen)
   ins.ecdf <- ecdf(ins)
   r<-range(c(sen,ins))
@@ -40,9 +38,24 @@ EP.plt<-function(X1, X2, colval, main){
        xlim=r, ylim=c(0,1))
 }
 
+EP.plt2<-function(X1, X2, colval, main){
+  sen<-X1[,ncol(setpt1.df)+colval]
+  ins<-X2[,ncol(setpt1.df)+colval]
+  
+  sen.ecdf <- ecdf(sen)
+  ins.ecdf <- ecdf(ins)
+  r<-range(c(sen,ins))
+  plot(sen, 1-sen.ecdf(sen), col="red", xlab="", ylab="", main=main,
+       xlim=r, ylim=c(0,1)) 
+  
+  par(new=TRUE)
+  plot(ins, 1-ins.ecdf(ins), col="blue",
+       axes=FALSE, ann=FALSE,
+       xlim=r, ylim=c(0,1))
+}
 
-pdf(file="fig5.pdf", width = 8, height = 8)
-par(mfrow=c(3,3), mar = c(4, 2, 3, 1), oma = c(3,3,2,0))
+pdf(file="fig5.pdf", width = 8, height = 6)
+par(mfrow=c(2,3), mar = c(4, 2, 3, 1), oma = c(3,3,2,0))
 EP.plt(sim1,sim2,1, "Exhaled air, AUC")
 legend("bottomleft", c("High sensity parameter","Low sensity parameter"), cex=1, 
        col=c("red","blue"), pch=21,  bty="n");
@@ -51,14 +64,22 @@ EP.plt(sim3,sim4,7, "Exhaled air, AUC")
 EP.plt(sim1,sim2,2, "Blood, AUC")
 EP.plt(sim1,sim2,5, "Blood, C")
 EP.plt(sim3,sim4,8, "Blood, AUC")
-EP.plt(sim1,sim2,3, "Percent cumulative")
-EP.plt(sim1,sim2,6, "Percent cumulative")
-EP.plt(sim3,sim4,9, "Percent cumulative")
 mtext("Inhalation", NORTH<-3, line=0, adj=0.32, cex=1.2, outer=TRUE, col="grey20")
 mtext("Ingestion", NORTH<-3, line=0, adj=0.88, cex=1.2, outer=TRUE, col="grey20")
 mtext("Estimated  logarithm concentration at 48-hr or AUC during the period", South<-1, line=0, cex=1.0, outer=TRUE) 
 mtext("Exceedance probability", WEST<-2, line=1.5, cex=1.0, outer=TRUE) 
 dev.off()
 
-
+pdf(file="fig6.pdf", width = 8, height = 3)
+par(mfrow=c(1,3), mar = c(3,2,1, 1), oma = c(3,3,2,0))
+EP.plt2(sim1,sim2,3, "")
+legend("topright", c("High sensity parameter","Low sensity parameter"), cex=1, 
+       col=c("red","blue"), pch=21,  bty="n");
+EP.plt2(sim1,sim2,6, "")
+EP.plt2(sim3,sim4,9, "")
+mtext("Inhalation", NORTH<-3, line=0, adj=0.32, cex=1.2, outer=TRUE, col="grey20")
+mtext("Ingestion", NORTH<-3, line=0, adj=0.88, cex=1.2, outer=TRUE, col="grey20")
+mtext("Cumulative fraction", South<-1, line=0, cex=1.0, outer=TRUE) 
+mtext("Exceedance probability", WEST<-2, line=1.5, cex=1.0, outer=TRUE) 
+dev.off()
 
