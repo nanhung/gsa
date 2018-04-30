8308c4b6562b5ee4af0cf6f51d2d7e1366a3d84a
nanhung <nhsieh@cvm.tamu.edu>
2018-04-30 16:04:53 -0500

    efast test OK

--- multisensii.R
+++ multisensii.R
@@ -36,39 +36,53 @@ if (is.loaded("derivs", PACKAGE=mName))
   dyn.unload(paste0(mName,.Platform$dynlib.ext))
 dyn.load(paste(mName, .Platform$dynlib.ext, sep=""))
 
-times <- seq(from = 0.5, to = 24.5, by = 1)
+T <- c(0, seq(from = 0.5, to = 24.5, by = 1)) # 0 is necessary
 parameters<-initparms1comp()
 parameters <- c(1,0.5,0.5)
 
 initState <- initState1comp(parms=parameters)
 initState[1] <- 1
 
-Y <- ode(initState, times, parms = parameters, dllname = mName, nout=1,
-    func = "derivs1comp", jacfunc = "jac1comp", initfunc = "initmod1comp", outnames = "Ccompartment")
-Y[,"Ccompartment"]
 
-verhulst2 <- function(X, t = t)
+pbtk <- function(X, t = T)
 {
   out <- matrix(nrow = nrow(X), ncol = length(t), NA)
   for (i in 1:nrow(X))
   {
-    out[i, ] <- verhulst(X$K[i], X$Y0[i], X$a[i], t)
+    Y <- ode(initState, T, parms = as.numeric(X[i,]), dllname = mName, nout=1,
+             func = "derivs1comp", jacfunc = "jac1comp", 
+             initfunc = "initmod1comp", outnames = "Ccompartment")
+    
+    out[i, ] <- Y[,"Ccompartment"]
   }
   out <- as.data.frame(out)
   names(out) <- paste("t", t, sep = "")
   return(out)
 }
 
+q.arg = list(list(min = 0.7, max = 1.3), 
+             list(min = 0.1, max = 0.4),
+             list(min = 0.8, max = 1.2))
 
 
-multisensi(design = fast99, model = pbtk,
-           center = FALSE, reduction = NULL, analysis = analysis.sensitivity,
-           design.args=list( factors=c("vdist","ke","kgutabs"), n=1000, q = "qunif",
-                             q.arg = list(list(min = 0.7, max = 1.3), 
-                                          list(min = 0.1, max = 0.4),
-                                          list(min = 0.9, max = 1.2))),
-           analysis.args=list(keep.outputs=FALSE))
+x <- fast99(model = NULL, factors = c("K","Y0","a"), n = 1000,
+            q = "qunif", q.arg = q.arg)
+X<-x$X
 
+pbtk(X)
+
+pbtk.seq.fast <- multisensi(design = fast99, model = pbtk,
+                            center = FALSE, reduction = NULL, analysis = analysis.sensitivity,
+                            design.args=list( factors=c("vdist","ke","kgutabs"), n=1000, q = "qunif",
+                                              q.arg = list(list(min = 0.7, max = 1.3), 
+                                                           list(min = 0.1, max = 0.4),
+                                                           list(min = 0.8, max = 1.2))),
+                            analysis.args=list(keep.outputs=FALSE))
+
+pbtk.seq.fast
+print(pbtk.seq.fast,digits=2)
+plot(pbtk.seq.fast, normalized = TRUE, color = terrain.colors, gsi.plot = FALSE)
+title(xlab = "Time in half-decades")
 
 #####
 
