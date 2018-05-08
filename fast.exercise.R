6da522bfc6d31a18bcd44c32d43ae86bee9a8b03
nanhung <nhsieh@cvm.tamu.edu>
2018-05-07 22:23:41 -0500

    3comp

--- fast.exercise.R
+++ fast.exercise.R
@@ -1,8 +1,9 @@
-# devtools::install_github("nanhung/ViSA")
+# devtools::install_github("nanhung/rfast99")
 
 library(rfast99)
 library(dplyr)
 library(deSolve)
+library(httk)
 #library(ggplot2)
 # sensitivty
 
@@ -98,18 +99,26 @@ compile <- function (mName)
 mName = "pbtk1comp"
 compile(mName)
 
+
+
+times <- seq(from = 0.5, to = 24.5, by = 1)
+parameters <- initparms1comp()
+initState <- initState1comp(parms=parameters)
+initState[1] <- 1
+
+params <- httk::parameterize_1comp(chem.name = "theophylline")
+
+# 10% uncertainty
+LL <- 0.9
+UL <- 1.1
+
 q = "qunif"
-q.arg = list(list(min = 0.7, max = 1.3),
-             list(min = 0.1, max = 0.4),
-             list(min = 0.9, max = 1.2))
+q.arg = list(list(min = params$Vdist * LL, max = params$Vdist * UL),
+             list(min = params$kelim * LL, max = params$kelim * UL),
+             list(min = params$kgutabs * LL, max = params$kgutabs * UL))
 
 x<-rfast99(factors = c("vdist", "ke", "kgutabs"),
            n = 100, q = q, q.arg = q.arg, rep = 10, conf = 0.99)
-
-times <- seq(from = 0.5, to = 24.5, by = 1)
-parameters<-initparms1comp()
-initState <- initState1comp(parms=parameters)
-initState[1] <- 1
 
 y<-solve_fun(x, times, parameters = parameters, initState, outnames = "Ccompartment",
              dllname = mName, func = "derivs1comp", jacfunc = "jac1comp", initfunc = "initmod1comp")
@@ -120,16 +129,92 @@ tell2(x,y)
 check(x)
 check(x, SI = 0.05, CI = 0.05)
 
+plot(x)
 
 ####
 
+
+mName = "3compPBPKmodel"
+compile(mName)
+
+parameters <- initparms3comp()
+initState <- initState3comp(parms=parameters)
+initState[1] <- 1
+
+params <- httk::parameterize_3comp(chem.name = "theophylline")
+
+# 10% uncertainty
+LL <- 0.9
+UL <- 1.1
+
+# 20 parameters
 q = "qunif"
-q.arg = list(list(min = 0.7, max = 1.3),
-             list(min = 0.1, max = 0.4),
-             list(min = 0.9, max = 1.2))
+q.arg = list(list(min = params$BW * LL, max = params$BW * UL),
+             list(min = params$Clmetabolismc * LL, max = params$Clmetabolismc * UL),
+             list(min = params$kgutabs * LL, max = params$kgutabs * UL),
+             list(min = params$Qcardiacc * LL, max = params$Qcardiacc * UL),
+             list(min = params$Qgfrc * LL, max = params$Qgfrc * UL),
+             list(min = params$Qgutf * LL, max = params$Qgutf * UL),
+             list(min = params$Qliverf * LL, max = params$Qliverf * UL),
+             list(min = params$Vgut * LL, max = params$Vgut * UL),
+             list(min = params$Vliver * LL, max = params$Vliver * UL),
+             list(min = params$Vrest * LL, max = params$Vrest * UL),
+             list(min = params$Funbound.plasma * LL, max = params$Funbound.plasma * UL),
+             list(min = params$Clmetabolism * LL, max = params$Clmetabolism * UL),
+             list(min = params$Qcardiac * LL, max = params$Qcardiac * UL),
+             list(min = params$Qgfr * LL, max = params$Qgfr * UL),
+             list(min = params$Qgut * LL, max = params$Qgut * UL),
+             list(min = params$Qliver * LL, max = params$Qliver * UL),
+             list(min = params$Kliver2pu * LL, max = params$Kliver2pu * UL),
+             list(min = params$Krest2pu * LL, max = params$Krest2pu * UL),
+             list(min = params$Kgut2pu * LL, max = params$Kgut2pu * UL),
+             list(min = params$Rblood2plasma * LL, max = params$Rblood2plasma * UL))
+
+factors <- names(parameters)
+
+x<-rfast99(factors = factors, n = 1000, q = q, q.arg = q.arg, rep = 10, conf = 0.99)
+
+times <- seq(from = 0.5, to = 24.5, by = 1)
+y<-solve_fun(x, times, parameters = parameters, initState, outnames = "Crest",
+             dllname = mName, func = "derivs3comp", jacfunc = "jac3comp", initfunc = "initmod3comp")
+
+tell2(x,y)
 
 
+check(x)
+check(x, SI = 0.05, CI = 0.05)
 
+plot(x)
+
+plot.rfast99 <- function(x, ...){
+  
+  nv <- length(colnames(x$tSI))+1
+  nc <- ceiling(sqrt(nv))
+  nr <- nv/nc
+  
+  par(mfrow = c(nr, nc), mar = c(4,2,4,1))
+  for(i in 1:ncol(x$tSI)){
+    plot(times, x$tSI[,i], ylim = c(0, 1), bty = 'n',
+         type = 'l', lwd = 2, xlab = 'time', ylab = '', 
+         main = colnames(x$tSI)[i])
+    col.transp = adjustcolor('black', alpha = 0.4)
+    polygon(x = c(times, rev(times)),
+            y =c(x$tSI[,i]-x$tCI[,i], rev(x$tSI[,i]+x$tCI[,i])),
+            col = col.transp, border = col.transp)
+    
+    col.transp = adjustcolor('red', alpha = 0.4)
+    lines(times, x$mSI[,i], ylim = c(0, 1), bty = 'n',
+          lwd = 2, col = 'red')
+    polygon(x = c(times, rev(times)),
+            y =c(x$mSI[,i]-x$mCI[,i], rev(x$mSI[,i]+x$mCI[,i])),
+            col = col.transp, border = col.transp)
+  }
+  plot.new()
+  legend('top', legend = c('total order', 'first order'), col = c('black','red'),
+         lty = 'solid', lwd = 1, pch = NA, bty = 'n',
+         text.col = 'black', 
+         fill = adjustcolor(c('black', 'red'), alpha = 0.4), border = NA, cex = 1.2)
+}
 
 
 ## Check each time point; time = 0.5
