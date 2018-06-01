b9cbb2aef1f3e3a43f496614ab26c4f537022714
nanhung <nhsieh@cvm.tamu.edu>
2018-05-31 20:46:42 -0500

    update

--- solvefun.R
+++ solvefun.R
@@ -1,12 +1,14 @@
 # system.time(source("solvefun.R"))
-#y<-solve_fun(x, times, parameters = parameters, initParmsfun = "initParms", 
-#             initState = initState, outnames = outnames, dllname = mName,
-#             func = "derivs", initfunc = "initmod", output = output, method = "lsode",
-#             initforc="initforc", forcings=Forcings1)
-y<-solve_MCSim(x, mName = mName,
-               infile.name = infile.name, 
-               outfile.name = outfile.name, 
-               parameters = factors,
-               output = output,
-               time = times, 
-               condition = conditions)
+y<-solve_fun(x, times, parameters = parameters, initParmsfun = "initParms", 
+             initState = initState, outnames = outnames, dllname = mName,
+             func = "derivs", initfunc = "initmod", output = output, method = "lsode",
+             initforc="initforc", forcings=Forcings1)
+
+# user   system  elapsed 
+# 4517.225    0.823 4518.596 
+
+y<-solve_MCSim(x, mName = mName, infile.name = infile.name, outfile.name = outfile.name, 
+               parameters = factors, output = output, time = times, condition = conditions)
+
+# user   system  elapsed 
+# 1267.084  652.133 1917.641 
