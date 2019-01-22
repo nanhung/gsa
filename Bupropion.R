232b4e1bb9d2d78b5f18ff60f0e4bedc4567a1b3
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-01-22 16:36:57 -0600

    revise the definition of V_central

--- Bupropion.R
+++ Bupropion.R
@@ -1,3 +1,6 @@
+# https://link.springer.com/article/10.1208/s12248-017-0102-8
+# https://link.springer.com/article/10.1007/s13318-018-0537-z
+
 library(pksensi)
 mName <- "acat2cpt_v3.0"
 compile_model(mName, app = "mcsim")
@@ -16,14 +19,17 @@ Solubility <- 312e6
 
 params <- c("K_precip", 
             "lnPeff", 
-            "lnV_central")
+            "lnV_central",
+            "lnKelim")
+
 q <- "qunif"
 q.arg <-list(list(0.72, 7.2), 
              list(-7.8, -4),
-             list(log(0.3), log(0.7)))
+             list(6, 7),
+             list(-3.2, -1.7)) # half lives 10-15 hr
 #times <- c(0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8, 24, 48)
 times <- seq(0.01, 49.01, 1)
-output <- c("C_central")
+output <- c("C_central", "Q_periph", "Q_liver", "Q_gi_lu_d", "Q_gi_ep", "Q_gi_w", "Q_total")
 set.seed(1234)
 x <- rfast99(params = params, n = 512, q = q, q.arg = q.arg, replicate = 1) 
 
@@ -70,7 +76,10 @@ fix_params <- c("f_Abs_stom = 1",
                 "G_delayed_u  = 0",
                 "lnKm_met_vitro = 1.824549",
                 "lnVmax_met_vitro = -13",
-                "lnKelim = -0.69")
+                "lnKe_over_a_epit = -6.9",
+                "lnKe_over_a_tiss = 0",
+                "lnKc2p = -13",
+                "lnKp2c = 0")
 give_dose <- "Oral_dose_rate = NDoses(2, 3128.35 0, 0 0.1)" # 75mg
 
 conditions <- c(fix_params, give_dose, 
@@ -90,5 +99,17 @@ y <- solve_mcsim(x, mName = mName,
                  condition = conditions,
                  generate.infile = F)
 
-pksim(y)
+#par(mfrow = c(2,3))
+pksim(y, var = 1)
 points(df$t, df$C/MW) # uM
+#pksim(y, var = 2)
+#pksim(y, var = 3)
+#pksim(y, var = 4)
+#pksim(y, var = 5)
+#pksim(y, var = 6)
+#pksim(y, var = 7)
+
+
+
+#pksim(y, log = T)
+#points(df$t, log(df$C/MW)) # uM
